003612ff83646c5017966591565659725cf0fecc
Guilherme Miyata <miyatagui120600@gmail.com>
2025-04-19 22:14:18 -0300

    ✨ feat: add SearchBar widget to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchBar from '@/widgets/SearchBar';
 import Header from '../widgets/Header';
 
 export default function Home() {
@@ -5,11 +6,12 @@ export default function Home() {
     <>
       <div>
         <Header />
+        <SearchBar />
       </div>
 
-      <hr />
+      <hr className="mt-5" />
 
-      <div className="border-green-600 border-2">Área conteúdos</div>
+      <div className="border-green-600 mt-10 border-2">Área conteúdos</div>
       <div className="border-purple-600 border-2">Rodapé</div>
     </>
   );
