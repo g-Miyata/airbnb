54f3669a5cb91f00d784167da1e47e3a4d507d31
Guilherme Miyata <miyatagui120600@gmail.com>
2025-04-21 17:42:09 -0300

    ✨ feat: create API types for accommodations, photos, testimonials, and locations

---
+++ src/types/AirbnbData.ts
@@ -0,0 +1,43 @@
+export interface AirbnbAPI {
+  accommdation: Accommodation[];
+  icons: Photo[];
+}
+
+export interface Accommodation {
+  id: string;
+  title: string;
+  date: string;
+  testimonials: Testimonials[];
+  hasBadge: boolean;
+  rating: number;
+  price: number;
+  host: string;
+  location: Location;
+  slug: string;
+  photos: Photo[];
+}
+
+export interface Testimonials {
+  id: string;
+  name: string;
+  image: string;
+  comment: string;
+  rating: number;
+  customerTime: number;
+  createdAt: number;
+  stayedAt: number;
+}
+
+export interface Photo {
+  id: string;
+  source: string;
+  description: string;
+  url: string;
+}
+
+export interface Location {
+  city: string;
+  state: string;
+  country: string;
+  description: string;
+}
