01d2e5a81c71fa69794bac67fdc4722b1ad1d6cb
Guilherme Miyata <miyatagui120600@gmail.com>
2025-04-21 18:36:15 -0300

    ✨ feat: create fetchDataBySlug function to get accommodation details

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,21 @@
-import { AirbnbAPI } from '@/types/AirbnbData';
+import { Accommodation, AirbnbAPI } from '@/types/AirbnbData';
 
 export async function fetchData(): Promise<AirbnbAPI> {
   try {
     const response = await fetch('https://web.codans.com.br/airbnb');
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+export async function fetchDataBySlug(slug: string): Promise<Accommodation | null> {
+  try {
+    const data = await fetchData();
+    const accommodation = data.accommodation.find((item) => item.slug === slug);
+    return accommodation || null;
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
